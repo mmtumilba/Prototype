d2338d3976412757d67fe0d1753a5aaf039771da
mmtumilba <mbtumilba1@up.edu.ph>
2021-01-07 06:26:34 +0800

    napapasa data from CategorySelectionActivity to ArticleSelectionActivity for abs-cbn

--- app/src/main/java/com/abc/prototype/ArticleSelectionActivity.java
+++ app/src/main/java/com/abc/prototype/ArticleSelectionActivity.java
@@ -3,12 +3,26 @@ package com.abc.prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ArticleSelectionActivity extends AppCompatActivity {
+
+    private String source;
+    private String category;
+    private String subcategory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_selection);
+
+
+        Bundle extras = getIntent().getExtras();
+        source = extras.getString("source");
+        category = extras.getString("category");
+        subcategory = extras.getString("subcategory");
+
+
     }
 }

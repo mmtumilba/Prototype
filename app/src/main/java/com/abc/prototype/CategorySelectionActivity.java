dc9b775cd8942b083fba745102cf981d4d755e0a
mmtumilba <mbtumilba1@up.edu.ph>
2021-01-07 02:49:36 +0800

    CategorySelectioActivity: back and next button for abs-cbn category selection

--- app/src/main/java/com/abc/prototype/CategorySelectionActivity.java
+++ app/src/main/java/com/abc/prototype/CategorySelectionActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -17,12 +18,25 @@ public class CategorySelectionActivity extends AppCompatActivity {
     private TextView tv;
     private EditText et;
     private TextToSpeech mTTS;
-//    private String source;
+
+    private Button btnBack;
+    private Button btnNext;
+    private Button btnSubmit;
+
+
+    private String source;
+    private String category;
+    private String subcategory;
+
+    private boolean hasSubcategory = false;
 
     private final String ABS = "abs";
     private final String GMA = "gma";
     private final String INQUIRER = "inquirer";
     private final String PHILSTAR = "philstar";
+
+    private int setNum = 1;
+    private int setMax;
 
 
     @Override
@@ -30,19 +44,109 @@ public class CategorySelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_selection);
 
-        Log.e("CHECKPOINT", "CategorySelectionActivity");
-
-
         Context context = getApplicationContext();
         mTTS = TextReader.initialize(context);
         tv = findViewById(R.id.textViewChooseCategory);
 
         Bundle extras = getIntent().getExtras();
-        String source = extras.getString("source");     // TODO: 05/01/2021 this may produce null pointerException
-        tv.setText(source);
+        source = extras.getString("source");     // TODO: 05/01/2021 this may produce null pointerException -> di na kasi sa prev activity, sinigurado nang hindi makakalusot pag walang laman
+        setTv();
 
+        btnBack = findViewById(R.id.buttonCategorySelectionBack);
+        btnBack.setAlpha((float) 0.5);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (source) {
+                    case ABS:
+                        absBackButton();
+                        break;
+                    case GMA:
+                        break;
+
+                    case INQUIRER:
+                        break;
+
+                    case PHILSTAR:
+                        break;
+
+                }
+
+            }
+        });
+
+        btnNext = findViewById(R.id.buttonCategorySelectionNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (source) {
+                    case ABS:
+                        absNextButton();
+                        break;
+                    case GMA:
+                        break;
+
+                    case INQUIRER:
+                        break;
+
+                    case PHILSTAR:
+                        break;
+
+                }
+            }
+        });
+
+
+        btnSubmit = findViewById(R.id.buttonCategorySelectionSubmit);
 
     }
+
+    private void absBackButton () {
+            setNum--;
+            btnBack.setAlpha((float) 0.5);
+            btnNext.setAlpha((float) 1);
+
+            tv.setText(R.string.abs_categories1);
+    }
+
+    private void absNextButton() {
+            setNum++;
+            btnBack.setAlpha((float) 1);
+            btnNext.setAlpha((float) 0.5);
+
+            tv.setText(R.string.abs_categories2);
+
+    }
+
+    private void setTv () {
+
+        switch (source) {
+            case ABS:
+                tv.setText(R.string.abs_categories1);
+                setMax = 2;
+                break;
+
+            case GMA:
+                hasSubcategory = true;
+                tv.setText(R.string.gma_categories1);
+                setMax = 3;
+                break;
+
+            case INQUIRER:
+                hasSubcategory = true;
+                tv.setText(R.string.inquirer_categories1);
+                setMax = 2;
+                break;
+
+            case PHILSTAR:
+                tv.setText(R.string.philstar_categories1);
+                setMax = 2;
+                break;
+
+        }
+    }
+
 }
 
 
--- app/src/main/java/com/abc/prototype/Navigate.java
+++ app/src/main/java/com/abc/prototype/Navigate.java
@@ -15,25 +15,19 @@ public class Navigate {
 
 
     public static void goToCategorySelectionActivity (Context context, String sourceStr) {
-        Log.e("CHECKPOINT", "nakapasok CategorySelectionActivity");
-
         Intent intent = new Intent(context, CategorySelectionActivity.class);
         intent.putExtra("source", sourceStr);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
-
-        Log.e("CHECKPOINT", "palabas ng navigate to CategorySelectionActivity");
-
     }
 
-    /*
-     * public static void goToArticleSelectionActivity (Context context, String categoryStr, String subcategoryStr) {
-     *         Intent intent = new Intent(context, CategorySelectionActivity.class);
-     *         intent.putExtra("category", categoryStr);
-     *         intent.putExtra("subcategory", subcategoryStr);
-     *         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-     *         context.startActivity(intent);
-     *
-     * }
-     */
+
+     public static void goToArticleSelectionActivity (Context context, String categoryStr, String subcategoryStr) {
+         Intent intent = new Intent(context, CategorySelectionActivity.class);
+         intent.putExtra("category", categoryStr);
+         intent.putExtra("subcategory", subcategoryStr);
+         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+         context.startActivity(intent);
+     }
+
 }

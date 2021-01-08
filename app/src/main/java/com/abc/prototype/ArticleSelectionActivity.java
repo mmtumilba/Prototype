ff8d8225c035c699bb8e3980ffe909ea2c2978fc
mmtumilba <mbtumilba1@up.edu.ph>
2021-01-08 13:21:17 +0800

    benchmark: CategorySelectionActivity done and functional

--- app/src/main/java/com/abc/prototype/ArticleSelectionActivity.java
+++ app/src/main/java/com/abc/prototype/ArticleSelectionActivity.java
@@ -11,13 +11,12 @@ public class ArticleSelectionActivity extends AppCompatActivity {
     private String source;
     private String category;
     private String subcategory;
-
+    private String link = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_selection);
-
 
         Bundle extras = getIntent().getExtras();
         source = extras.getString("source");         // TODO: 05/01/2021 this may produce null pointerException -> di na kasi sa prev activity, sinigurado nang hindi makakalusot pag walang laman
@@ -27,7 +26,6 @@ public class ArticleSelectionActivity extends AppCompatActivity {
         TextView tv = findViewById(R.id.category);
         tv.setText(category);
         Log.e("source", source);
-
 
     }
 }
